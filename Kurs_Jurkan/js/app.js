1d87a1063c030005d2895869218160d910ce4803
Kacper Kwietniewski <kkwietniewski@edu.cdv.pl>
2020-05-24 19:52:13 +0200

    Test

--- Kurs_Jurkan/js/app.js
+++ Kurs_Jurkan/js/app.js
@@ -3,7 +3,8 @@ var tasksContainer = document.querySelector('.tasks-container ul');
 var newTaskForm = document.querySelector('.new-task-container form');
 
 //On DOM load
-document.addEventListener('DOMContentLoaded',function(){
+document.addEventListener('DOMContentLoaded', function () {
 	bindAddTaskEvents();
 	showTasks();
+
 });
